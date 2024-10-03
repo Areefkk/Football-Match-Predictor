4e09509ff2efc360d3d126f6fe037ca39b5772e3
Areef Khan <areef_khan@outlook.com>
2024-10-03 23:29:47 +0100

    soup created for HTML extraction

--- match_predictor.py
+++ match_predictor.py
@@ -2,3 +2,11 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+## Creates a soup to extract the HTML code for the website
+url = 'https://en.wikipedia.org/wiki/2022_FIFA_World_Cup'
+res = requests.get(url)
+content = res.text
+soup = BeautifulSoup(content, 'lxml')
+
+match_data = soup.find_all('div', class_='footballbox')
+
