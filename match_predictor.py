e78a484a64680a2f3adcd4f5890d709757fb446f
Areef Khan <areef_khan@outlook.com>
2024-10-06 22:35:48 +0100

    function created for calculating points

--- match_predictor.py
+++ match_predictor.py
@@ -10,3 +10,20 @@ soup = BeautifulSoup(content, 'lxml')
 
 match_data = soup.find_all('div', class_='footballbox')
 
+## Extracts the data/scores for the home and away teams
+for match in match_data:
+    home_team.append(match.find('th', class_='fhome').get_text())
+    score.append(match.find('th', class_='fscore').get_text())
+    away_team.append(match.find('th', class_='faway').get_text())
+
+dict_football = {'home_team': home_team, 'score': score, 'away_team': away_team}
+df_football = pd.DataFrame(dict_football)
+
+df_football.to_csv("fifa_worldcup_data.csv", index=False)
+
+def predict(home_team, away_team):
+
+    # Calculate the value of lambda (λ) for both Home Team and Away Team.
+    if home_team in df_football.index and away_team in df_football.index:
+        lambda_home_team = df_football.at[home_team,'GoalsScored'] * df_football.at[away_team,'GoalsConceded']
+        lambda_away_team = df_football.at[away_team,'GoalsScored'] * df_football.at[home_team,'GoalsConceded']
